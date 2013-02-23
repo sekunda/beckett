3738428d5f475a897335d798e43c8f52accfccc3
Barbara Szczygieł <barbara.szczygiel@gmail.com>
2013-02-23 16:24:15 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beckett::Application.config.secret_token = '3d4f29c710e683922ba7708517a452f2b3f0ee7d70bedd8672b7f6cadf2c37a334318ccaa5ea5d28d9d850949de508a290cd2a688f5f5a1246a1e5ba80e41f2b'
